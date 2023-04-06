6b5f086c189a4209926a689d6debf3e93b1d4f53
Scarlett Borroel <scarlett@Scarletts-MacBook-Pro.local>
2023-04-05 23:14:54 -0700

    added attributes to JS

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var hourNine = document.getElementById("hour-9");
-var hourTen = document.getElementById("hour-10");
-var hourEleven = document.getElementById("hour-11");
-var hourTwelve = document.getElementById("hour-12");
-var hourOne = document.getElementById("hour-1");
-var hourTwo = document.getElementById("hour-2");
-var hourThree = document.getElementById("hour-3");
-var hourFour = document.getElementById("hour-4");
-var houtFive = document.getElementById("hour-5");
+var $container = $('.container-lg.px5');
+var $hours = $container.find('[id^="hour-"]');
 
-$(function () {
+$(document).ready(function () {
+  $("#myButton").click(function() {
+    $(this).attr(localStorage)
+  });
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -24,15 +20,24 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  $("9r").attr("data-time", moment("9:00 am", "h:mm a").format("HH"));
+  $("10r").attr("data-time", moment("10:00 am", "h:mm a").format("HH"));
+  $("11r").attr("data-time", moment("11:00 am", "h:mm a").format("HH"));
+  $("12r").attr("data-time", moment("12:00 pm", "h:mm a").format("HH"));
+  $("1r").attr("data-time", moment("1:00 pm", "h:mm a").format("HH"));
+  $("2r").attr("data-time", moment("2:00 pm", "h:mm a").format("HH"));
+  $("3r").attr("data-time", moment("3:00 pm", "h:mm a").format("HH"));
+  $("4r").attr("data-time", moment("4:00 pm", "h:mm a").format("HH"));
+  $("5r").attr("data-time", moment("5:00 pm", "h:mm a").format("HH"));
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+  
   //
   // TODO: Add code to display the current date in the header of the page.
 });
-
-
 
 function updateClock() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
